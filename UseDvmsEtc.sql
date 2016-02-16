8df5a347320f79a930844db0ef7e73e877b344c3
aadennis <dennis.circ@gmail.com>
2016-02-16 22:48:51 +0000

    initial sql helper scripts re dmv
    initial sql helper scripts re dmv

---
+++ UseDvmsEtc.sql
@@ -0,0 +1,19 @@
+-- For each database, count rows in non-empty tables,
+-- ordering by rowcount desc per database
+
+declare @sql nvarchar(1000) = 'use ?; 
+	select	db_name() as DatabaseName,
+		object_name([ddps].[object_id]) as ObjectName,
+		[ddps].[row_count]
+	from 
+		[sys].[dm_db_partition_stats] as ddps
+	join
+		[sys].[objects] as o
+	on	[ddps].object_id = [o].object_id
+	where
+		--[ddps].[row_count] > 0 and
+		ddps.index_id = 1 and 
+		[o].[type] = ''U''
+	order by [ddps].[row_count] desc'
+
+exec master.dbo.sp_msforeachdb @command1 = @sql;
