36bf603bd6dbe8d46624db84dadc62037357696d
aadennis <dennis.circ@gmail.com>
2016-11-19 22:59:49 +0000

    Notes about Import-Module

--- BackupAndRestoreDb.sql
+++ BackupAndRestoreDb.sql
@@ -1,5 +1,13 @@
+#https://msdn.microsoft.com/en-us/library/ms205154.aspx
+
+#If Import-Module line does not work do this (next line assumes SQLServer 2016 - adjust for you)...
+$env:PSModulePath = $env:PSModulePath + ";C:\Program Files (x86)\Microsoft SQL Server\130\Tools\PowerShell\Modules"
+Get-Module -ListAvailable
+
+$serverName = "dennis-pc"
 Import-Module SQLPS -DisableNameChecking
-set-location SQLSERVER:\SQL\denmot\default\databases
+set-location SQLSERVER:\SQL
+set-location SQLSERVER:\SQL\$serverName\default\databases
 ls
 
 $now = Get-Date -Format yyyyMMddHHmmss
@@ -8,10 +16,16 @@ gci | % {
     $db = $_.Name
     $backupName = "f:\dump\$db.$now.bak"
     write-host $backupName
-    Backup-SqlDatabase -ServerInstance denmot -Database $db -BackupFile $backupName
+    Backup-SqlDatabase -ServerInstance $serverName -Database $db -BackupFile $backupName
 }
 
 $db = 'WideWorldImporters'
 $backupFile = "f:\dump\WideWorldImporters.20161118201409.bak"
-Restore-SqlDatabase -ServerInstance denmot -Database $db -BackupFile $backupFile -ReplaceDatabase
+Restore-SqlDatabase -ServerInstance $serverName -Database $db -BackupFile $backupFile -ReplaceDatabase
 
+$srv = new-Object Microsoft.SqlServer.Management.Smo.Server("(local)")
+$db = New-Object Microsoft.SqlServer.Management.Smo.Database($srv, "Test_SMO_Database")
+$db.Create()
+Write-Host $db.CreateDate
+ls
+#$db.Drop()
