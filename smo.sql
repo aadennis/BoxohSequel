38474e504be83ecf1ab384ba785f58aa876d01a8
aadennis <dennis.circ@gmail.com>
2016-11-19 05:19:04 +0000

    do it

---
+++ smo.sql
@@ -0,0 +1,127 @@
+##########################################################
+# SMO object model
+# https://msdn.microsoft.com/en-us/library/ms162151.aspx
+##########################################################
+
+
+
+#https://msdn.microsoft.com/en-us/library/ms162157.aspx
+
+# Set the path context to the local, default instance of SQL Server.  
+CD \sql\localhost\  
+$srv = get-item default  
+  
+#Display information about the instance of SQL Server in Information and Settings.  
+"OS Version = " + $srv.Information.OSVersion  
+"State = "+ $srv.Settings.State.ToString()  
+  
+#Display information specific to the current user in UserOptions.  
+"Quoted Identifier support = " + $srv.UserOptions.QuotedIdentifier  
+  
+#Modify server settings in Settings.  
+$srv.Settings.LoginMode = [Microsoft.SqlServer.Management.SMO.ServerLoginMode]::Integrated  
+  
+#Modify settings specific to the current connection in UserOptions.  
+$srv.UserOptions.AbortOnArithmeticErrors = $true  
+  
+#Run the Alter method to make the changes on the instance of SQL Server.  
+$srv.Alter()  
+
+#Get a server object which corresponds to the default instance replace LocalMachine with the physical server  
+cd \sql\localhost\  
+$svr = get-item default  
+  
+#enumerate its properties  
+foreach ($Item in $Svr.Configuration.Properties)   
+{  
+ $Item.DisplayName  
+}  
+  
+"There are " + $svr.Configuration.Properties.Count.ToString() + " configuration options."  
+  
+#Display the maximum and minimum values for ShowAdvancedOptions.  
+$min = $svr.Configuration.ShowAdvancedOptions.Minimum  
+$max = $svr.Configuration.ShowAdvancedOptions.Maximum  
+"Minimum and Maximum values are " + $min.ToString() + " and " + $max.ToString() + "."  
+  
+#Modify the value of ShowAdvancedOptions and run the Alter method.  
+$svr.Configuration.ShowAdvancedOptions.ConfigValue = 0  
+$svr.Configuration.Alter()  
+  
+#Display when the change takes place according to the IsDynamic property.  
+If ($svr.Configuration.ShowAdvancedOptions.IsDynamic -eq $true)  
+ {    
+   "Configuration option has been updated."  
+ }  
+Else  
+{  
+    "Configuration option will be updated when SQL Server is restarted."  
+}  
+
+#https://msdn.microsoft.com/en-us/library/ms162577.aspx
+#Get a server object which corresponds to the default instance  
+cd \sql\localhost\  
+$srv = get-item default  
+  
+#Create a new database  
+$db = New-Object -TypeName Microsoft.SqlServer.Management.Smo.Database -argumentlist $srv, "Test_SMO_Database2"  
+$db.Create()  
+  
+#Reference the database and display the date when it was created.   
+$db = $srv.Databases["Test_SMO_Database2"]  
+$db.CreateDate  
+  
+#Drop the database  
+$db.Drop()  
+
+#https://msdn.microsoft.com/en-us/library/ms162575.aspx
+#Load the assembly containing the objects used in this example  
+[reflection.assembly]::LoadWithPartialName("Microsoft.SqlServer.Types")  
+  
+# Set the path context to the local, default instance of SQL Server.  
+CD \sql\localhost\default\Databases\  
+  
+#And the database object corresponding to AdventureWorks2012.  
+$db = get-item AdventureWorks2012  
+  
+#Create a SMO Table  
+$tb = New-Object -TypeName Microsoft.SqlServer.Management.SMO.Table -argumentlist $db, "Test_Table"  
+  
+#Add various columns to the table.   
+$Type = [Microsoft.SqlServer.Management.SMO.DataType]::NChar(50)  
+$col1 =  New-Object -TypeName Microsoft.SqlServer.Management.SMO.Column -argumentlist $tb,"Name", $Type  
+$col1.Collation = "Latin1_General_CI_AS"  
+$col1.Nullable = $true  
+$tb.Columns.Add($col1)  
+  
+$Type = [Microsoft.SqlServer.Management.SMO.DataType]::Int  
+$col2 =  New-Object -TypeName Microsoft.SqlServer.Management.SMO.Column -argumentlist $tb,"ID", $Type  
+$col2.Identity = $true  
+$col2.IdentitySeed = 1  
+$col2.IdentityIncrement = 1  
+$tb.Columns.Add($col2)   
+  
+$Type = [Microsoft.SqlServer.Management.SMO.DataType]::Real  
+$col3 =  New-Object -TypeName Microsoft.SqlServer.Management.SMO.Column -argumentlist $tb,"Value", $Type  
+$tb.Columns.Add($col3)   
+  
+$Type = [Microsoft.SqlServer.Management.SMO.DataType]::DateTime  
+$col4 =  New-Object -TypeName Microsoft.SqlServer.Management.SMO.Column -argumentlist $tb,"Date", $Type  
+$col4.Nullable = $false  
+$tb.Columns.Add($col4)   
+  
+#Create the table  
+$tb.Create()  
+  
+$Type = [Microsoft.SqlServer.Management.SMO.DataType]::DateTime  
+$col5 =  New-Object -TypeName Microsoft.SqlServer.Management.SMO.Column -argumentlist $tb,"ExpiryDate", $Type  
+$col5.Nullable = $false  
+$tb.Columns.Add($col5)   
+  
+#Run the Alter method to make the change on the instance of SQL Server.   
+$tb.Alter()  
+  
+#Remove the table from the database.   
+$tb.Drop()  
+  
+
